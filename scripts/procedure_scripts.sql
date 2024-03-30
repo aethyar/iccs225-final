55755e1875dfd28634058c282ef97f9966fe33bd
Aethyar <aethyar@gmail.com>
2024-03-31 03:48:35 +0700

    Fix forgotten update on ingredient table field

--- scripts/procedure_scripts.sql
+++ scripts/procedure_scripts.sql
@@ -9,11 +9,11 @@ END;
 $$;
 
 -- procedure to update ingredient information
-CREATE OR REPLACE PROCEDURE update_ingredient_info(cur_ingredient_id INT, new_name VARCHAR(100), new_description TEXT, new_category VARCHAR(50), new_quantity INT, new_expiry_date DATE, new_alert_threshold INT)
+CREATE OR REPLACE PROCEDURE update_ingredient_info(cur_ingredient_id INT, new_name VARCHAR(100), new_description TEXT, new_category VARCHAR(50), new_stock INT, new_expiry_date DATE, new_alert_threshold INT)
 LANGUAGE plpgsql AS $$
 BEGIN
     UPDATE "ingredients"
-    SET "name" = new_name, "description" = new_description, "category" = new_category, "quantity" = new_quantity, "expiry_date" = new_expiry_date, "alert_threshold" = new_alert_threshold
+    SET "name" = new_name, "description" = new_description, "category" = new_category, "stock" = new_stock, "expiry_date" = new_expiry_date, "alert_threshold" = new_alert_threshold
     WHERE "ingredient_id" = cur_ingredient_id;
 END;
 $$;
