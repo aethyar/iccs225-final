05bdfee5785d0d79550988fd8d236a5886b9efdc
Aethyar <aethyar@gmail.com>
2024-03-31 14:20:14 +0700

    Add function scripts for removing and updating in the Food DB

--- scripts/function_scripts.sql
+++ scripts/function_scripts.sql
@@ -45,6 +45,111 @@ END;
 $$
     LANGUAGE plpgsql;
 
+-- function for removing a food
+CREATE OR REPLACE FUNCTION remove_food(id INT)
+    RETURNS VOID AS
+$$
+BEGIN
+    DELETE FROM foods WHERE id = food_id;
+END;
+$$
+    LANGUAGE plpgsql;
+
+-- function for removing an ingredient
+CREATE OR REPLACE FUNCTION remove_ingredient(id INT)
+    RETURNS VOID AS
+$$
+BEGIN
+    DELETE FROM ingredients WHERE id = ingredient_id;
+END;
+$$
+    LANGUAGE plpgsql;
+
+-- function for removing a connection between food and ingredient
+CREATE OR REPLACE FUNCTION remove_ingredient_food_connection(f_id INT, i_id INT)
+    RETURNS VOID AS
+$$
+BEGIN
+    DELETE FROM ingredient_food WHERE f_id = food_id AND i_id = ingredient_id;
+END;
+$$
+    LANGUAGE plpgsql;
+
+-- function for removing a transaction
+CREATE OR REPLACE FUNCTION remove_transaction(id INT)
+    RETURNS VOID AS
+$$
+BEGIN
+    DELETE FROM transactions WHERE id = transaction_id;
+END;
+$$
+    LANGUAGE plpgsql;
+
+-- function for updating a food
+CREATE OR REPLACE FUNCTION update_food(id INT, new_name VARCHAR(100), new_description TEXT, new_category VARCHAR(50),
+                                       new_cost DECIMAL(10, 2), new_img_link VARCHAR(255))
+    RETURNS VOID AS
+$$
+BEGIN
+    UPDATE foods
+    SET name        = new_name,
+        description = new_description,
+        category    = new_category,
+        cost        = new_cost,
+        img_link    = new_img_link
+    WHERE id = food_id;
+END;
+$$
+    LANGUAGE plpgsql;
+
+-- function for updating an ingredient
+CREATE OR REPLACE FUNCTION update_ingredient(id INT, new_name VARCHAR(100), new_description TEXT,
+                                             new_category VARCHAR(50), new_stock INT, new_expiry_date DATE,
+                                             new_alert_threshold INT)
+    RETURNS VOID AS
+$$
+BEGIN
+    UPDATE ingredients
+    SET name            = new_name,
+        description     = new_description,
+        category        = new_category,
+        stock           = new_stock,
+        expiry_date     = new_expiry_date,
+        alert_threshold = new_alert_threshold
+    WHERE id = ingredient_id;
+END;
+$$
+    LANGUAGE plpgsql;
+
+-- function for updating a connection between food and ingredient
+CREATE OR REPLACE FUNCTION update_ingredient_food_connection(i_id INT, f_id INT, new_quantity INT)
+    RETURNS VOID AS
+$$
+BEGIN
+    UPDATE ingredient_food
+    SET quantity = new_quantity
+    WHERE i_id = f_id
+      AND food_id = food_id;
+END;
+$$
+    LANGUAGE plpgsql;
+
+-- function for updating a transaction
+CREATE OR REPLACE FUNCTION update_transaction(id INT, new_time TIMESTAMP, new_food_id INT, new_quantity INT,
+                                              new_total_price DECIMAL(10, 2))
+    RETURNS VOID AS
+$$
+BEGIN
+    UPDATE transactions
+    SET time        = new_time,
+        food_id     = new_food_id,
+        quantity    = new_quantity,
+        total_price = new_total_price
+    WHERE id = transaction_id;
+END;
+$$
+    LANGUAGE plpgsql;
+
 -- for updating staff info
 CREATE OR REPLACE FUNCTION update_staff_info(cur_staff_id INT, new_name VARCHAR(100), new_address TEXT,
                                              new_phone_number VARCHAR(20), new_email VARCHAR(100), new_hire_date DATE,
